ff2198e41a5c54e56aa793c65fe7ca18c8854352
Jasper Swallen <swallen@usc.edu>
2022-09-01 17:13:13 -0700

    sympy coulombs law eval

--- equations.py
+++ equations.py
@@ -1,10 +1,9 @@
 import sympy as sp
 
-from sympy.printing.preview import system_default_viewer
-
 import numpy as np
 
-COULOMB_CONSTANT, MU_NAUGHT, EPSILON_NAUGHT, r = sp.symbols("k, \\mu_{0}, \\epsilon_{0}, r")
+COULOMB_CONSTANT = (sp.symbols("k"), 1234)
+MU_NAUGHT, EPSILON_NAUGHT, r = sp.symbols("\\mu_{0}, \\epsilon_{0}, r")
 
 
 def coulombs_law_eqn(charge: float):
@@ -13,9 +12,22 @@ def coulombs_law_eqn(charge: float):
     charge ``charge``.
 
     @param charge The charge of the point charge
+
+    E = k|q|/r^2
     """
-    return eval("1/(4 * sp.pi * EPSILON_NAUGHT) * (abs(charge)) / r**2")
+
+    equation = "1/(4 * sp.pi * EPSILON_NAUGHT) * (abs(charge)) / r ** 2"
+
+    return eval(equation)
 
 
 sp.init_printing()
-sp.preview(sp.latex(coulombs_law_eqn(7)), output="png", viewer=system_default_viewer)
+
+res = coulombs_law_eqn(7)
+print(res)
+
+for r_val in range(1, 10, 1):
+    f = res.subs({r: r_val, EPSILON_NAUGHT: 1, sp.pi: np.pi})
+    print(f)
+
+sp.preview(sp.latex(res), output="png")
