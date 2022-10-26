155614bef3b2cff4a677153fe9dbc795407fce4b
Jasper Swallen <swallen@usc.edu>
2022-10-26 02:17:56 -0700

    use custom Point2D tuple for points
    also clean up some other typing stuff

--- src/equations/constants.py
+++ src/equations/constants.py
@@ -2,10 +2,21 @@
 Constants needed for calculating the electric field.
 """
 
+from typing import NamedTuple
+
 import numpy as np
 
 EPSILON_NOUGHT = 1 / (4 * np.pi * 10**-7 * 299792458**2)
+"""
+Vacuum permittivity (ε_0), in C^2 / N / m^2
+"""
+
 COULOMB_CONSTANT = 1 / (4 * np.pi * EPSILON_NOUGHT)
 """
-Coulomb's constant, in N m^2 / C^2.
+Coulomb's constant (k), in N m^2 / C^2.
+"""
+
+Point2D = NamedTuple("Point2D", [("x", float), ("y", float)])
+"""
+A 2-dimensional point, with x and y float positions.
 """
