e25c6212add58b23857a3046045db5bdff457cbe
Jasper Swallen <swallen@usc.edu>
2022-10-28 16:32:59 -0700

    extend to arbitrary number of lines

--- src/view/two_line_dialog.py
+++ src/view/two_line_dialog.py
@@ -2,28 +2,25 @@
 A QDialog with 2 input lines.
 """
 
-from typing import Optional, Tuple
+from typing import List, Optional, Tuple
 
 from PyQt6 import QtWidgets, QtCore
 
 
-class TwoLineDialog(QtWidgets.QDialog):
+class MultiLineInputDialog(QtWidgets.QDialog):
     """
     A QDialog with 2 input lines.
     """
 
-    def __init__(self,
-                 top_line_label: str = "",
-                 bottom_line_label: str = "",
-                 parent: Optional[QtWidgets.QWidget] = None) -> None:
+    def __init__(self, labels: List[str], parent: Optional[QtWidgets.QWidget] = None) -> None:
         super().__init__(parent)
 
         layout = QtWidgets.QFormLayout(self)
-        self.top_line_le = QtWidgets.QLineEdit(self)
-        layout.addRow(QtWidgets.QLabel(top_line_label), self.top_line_le)
 
-        self.bottom_line_le = QtWidgets.QLineEdit(self)
-        layout.addRow(QtWidgets.QLabel(bottom_line_label), self.bottom_line_le)
+        self.line_edits: List[QtWidgets.QLineEdit] = []
+        for label in labels:
+            self.line_edits.append(QtWidgets.QLineEdit(self))
+            layout.addRow(QtWidgets.QLabel(label), self.line_edits[-1])
 
         button_box = QtWidgets.QDialogButtonBox(
             QtWidgets.QDialogButtonBox.StandardButton.Ok |
@@ -35,29 +32,27 @@ class TwoLineDialog(QtWidgets.QDialog):
 
         self.setLayout(layout)
 
-    def get_doubles(self) -> Tuple[float, float, bool]:
+    def get_doubles(self) -> Tuple[List[float], bool]:
         """
-        Get both input lines as floats, as well as a bool demonstrating the status.
+        Get all input lines as floats, as well as a bool demonstrating the status.
 
-        If the dialog was accepted and both lines could both be successfully converted to floats,
+        If the dialog was accepted and all lines could be successfully converted to floats,
         then success is set to True. Otherwise, the success is False.
 
         Any floats that could not be converted (or on a rejected dialog) are set to NaN.
 
         Returns:
-            tuple(float, float, bool): The top line's value as a float, the bottom line's value as a
-            float, and the success as a bool.
+            tuple(List[float], bool): The every line's value as a float and the success as a bool.
         """
 
-        top, bottom, success = float("nan"), float("nan"), False
+        doubles, success = [float("nan") for _ in self.line_edits], False
 
         if self.exec() == QtWidgets.QDialog.DialogCode.Accepted:
-            try:
-                top = float(self.top_line_le.text())
-                bottom = float(self.bottom_line_le.text())
+            success = True
+            for i, line_edit in enumerate(self.line_edits):
+                try:
+                    doubles[i] = float(line_edit.text())
+                except ValueError:
+                    success = False
 
-                success = True
-            except ValueError:
-                pass
-
-        return top, bottom, success
+        return doubles, success
