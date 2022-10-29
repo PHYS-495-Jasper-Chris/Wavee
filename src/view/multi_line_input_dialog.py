3ac98a9c2e3ab982e617d023a866bb464a21f396
Jasper Swallen <swallen@usc.edu>
2022-10-29 16:07:28 -0700

    add point charge and inf line charge menus

--- src/view/multi_line_input_dialog.py
+++ src/view/multi_line_input_dialog.py
@@ -1,36 +1,32 @@
 """
-A QDialog with 2 input lines.
+A QDialog with multiple input lines.
 """
 
-from typing import List, Optional, Tuple
+from typing import List, Optional, Tuple, Type, TypeVar
 
 from PyQt6 import QtWidgets, QtCore
 
 
 class MultiLineInputDialog(QtWidgets.QDialog):
     """
-    A QDialog with 2 input lines.
+    A QDialog with multiple input lines.
     """
 
-    def __init__(self, labels: List[str], parent: Optional[QtWidgets.QWidget] = None) -> None:
+    LineType = TypeVar("LineType", bound=QtWidgets.QWidget)
+    """
+    A generic line edit type.
+    """
+
+    def __init__(self,
+                 labels: List[str],
+                 parent: Optional[QtWidgets.QWidget] = None,
+                 description: Optional[str] = None) -> None:
         super().__init__(parent)
 
-        layout = QtWidgets.QFormLayout(self)
+        self.labels = labels
+        self.description = description
 
-        self.line_edits: List[QtWidgets.QLineEdit] = []
-        for label in labels:
-            self.line_edits.append(QtWidgets.QLineEdit(self))
-            layout.addRow(QtWidgets.QLabel(label), self.line_edits[-1])
-
-        button_box = QtWidgets.QDialogButtonBox(
-            QtWidgets.QDialogButtonBox.StandardButton.Ok |
-            QtWidgets.QDialogButtonBox.StandardButton.Cancel, QtCore.Qt.Orientation.Horizontal,
-            self)
-        button_box.accepted.connect(self.accept)
-        button_box.rejected.connect(self.reject)
-        layout.addRow(button_box)
-
-        self.setLayout(layout)
+        self.lines: List[MultiLineInputDialog.LineType] = []
 
     def get_doubles(self) -> Tuple[List[float], bool]:
         """
@@ -42,17 +38,65 @@ class MultiLineInputDialog(QtWidgets.QDialog):
         Any floats that could not be converted (or on a rejected dialog) are set to NaN.
 
         Returns:
-            tuple(List[float], bool): The every line's value as a float and the success as a bool.
+            tuple(List[float], bool): Every line's value as a float and the success as a bool.
         """
 
-        doubles, success = [float("nan") for _ in self.line_edits], False
+        self._make_layout(QtWidgets.QDoubleSpinBox)
+
+        doubles, success = [float("nan") for _ in self.lines], False
 
         if self.exec() == QtWidgets.QDialog.DialogCode.Accepted:
             success = True
-            for i, line_edit in enumerate(self.line_edits):
+            for i, line_edit in enumerate(self.lines):
                 try:
                     doubles[i] = float(line_edit.text())
                 except ValueError:
                     success = False
 
         return doubles, success
+
+    def get_texts(self) -> Tuple[List[str], bool]:
+        """
+        Get all input lines as strings, as well as a bool demonstrating the status.
+
+        If the dialog was accepted, then success is set to True. Otherwise, the success is False.
+
+        Returns:
+            tuple(List[str], bool): Every line's text as a str and the success as a bool.
+        """
+
+        self._make_layout(QtWidgets.QLineEdit)
+
+        texts, success = ["" for _ in self.lines], False
+
+        if self.exec() == QtWidgets.QDialog.DialogCode.Accepted:
+            success = True
+
+            for i, line_edit in enumerate(self.lines):
+                texts[i] = line_edit.text()
+
+        return texts, success
+
+    def _make_layout(self, line_type: Type[LineType]) -> None:
+        """
+        Make the layout with a specific line type.
+        """
+
+        layout = QtWidgets.QFormLayout(self)
+
+        if self.description is not None:
+            layout.addRow(QtWidgets.QLabel(self.description, self))
+
+        for label in self.labels:
+            self.lines.append(line_type(self))
+            layout.addRow(QtWidgets.QLabel(label), self.lines[-1])
+
+        button_box = QtWidgets.QDialogButtonBox(
+            QtWidgets.QDialogButtonBox.StandardButton.Ok |
+            QtWidgets.QDialogButtonBox.StandardButton.Cancel, QtCore.Qt.Orientation.Horizontal,
+            self)
+        button_box.accepted.connect(self.accept)
+        button_box.rejected.connect(self.reject)
+        layout.addRow(button_box)
+
+        self.setLayout(layout)
